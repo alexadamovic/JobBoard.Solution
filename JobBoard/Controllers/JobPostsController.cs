1bd81cd83ab4663cef49ca2a5f1d1b9d91cfcb9f
Alex Adamovic <alexanderadamovic@gmail.com>
2022-03-02 14:35:55 -0800

    finish JobBoard Epicodus practice project
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Jase Seeley <jcseeley31590@yahoo.com>

--- JobBoard/Controllers/JobPostsController.cs
+++ JobBoard/Controllers/JobPostsController.cs
@@ -9,12 +9,33 @@ namespace JobBoard.Controllers
     [HttpGet("/jobposts")]
     public ActionResult Index()
     {
-      return View();
+      List<JobPost> allPosts = JobPost.GetAll();
+      return View(allPosts);
+    }
+
+    [HttpPost("/jobposts")]
+    public ActionResult Create(string title, string description, string contactInfo)
+    {
+      JobPost userPost = new JobPost(title, description, contactInfo);
+      return RedirectToAction("Index");
     }
     
     [HttpGet("/jobposts/new")]
     public ActionResult New()
     {
+      return View();
+    }
+
+    [HttpGet("/jobposts/{id}")]
+    public ActionResult Show(int id)
+    {
+      return View(JobPost.Find(id));
+    }
+
+    [HttpPost("/jobposts/delete")]
+    public ActionResult DeleteAll()
+    {
+      JobPost.ClearAll();
       return View();
     }
 
